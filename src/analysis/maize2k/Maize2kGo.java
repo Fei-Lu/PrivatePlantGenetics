3a7b39e340f8fa11f89a7b10c8e5615ebf7b7c23
feilu <dr.lufei@gmail.com>
2017-07-27 22:05:00 +0800

    main class for maize2k

---
+++ src/analysis/maize2k/Maize2kGo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analysis.maize2k;
+
+/**
+ *
+ * @author feilu
+ */
+public class Maize2kGo {
+    
+    public Maize2kGo () {
+        this.seqQualityTestPipe();
+    }
+    
+    public void seqQualityTestPipe () {
+        
+    }
+    
+    public static void main (String[] args) {
+        new Maize2kGo();
+    }
+    
+}
