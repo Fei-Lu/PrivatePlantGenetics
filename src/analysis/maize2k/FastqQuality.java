8ee8a97a3612d4f6c6e5742f43ad81cf21d4e9af
feilu <dr.lufei@gmail.com>
2017-08-02 22:53:10 +0800

    FastQC

--- src/analysis/maize2k/FastqQuality.java
+++ src/analysis/maize2k/FastqQuality.java
@@ -7,14 +7,18 @@ package analysis.maize2k;
 
 import format.Table;
 import graphcis.r.DensityPlot;
-import graphcis.r.Histogram;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import utils.IOUtils;
 
 /**
@@ -25,15 +29,114 @@ public class FastqQuality {
     
     public FastqQuality () {
         //this.covergage();
-        this.covergagePlot();
+        //this.covergagePlot();
         //this.sampleReads();
-        
+        //this.fastQC();
+        //this.fastQCsummary();
+    }
+    
+    private void fastQCsummary () {
+        String inputDirS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/fastQC/autoReport";
+        String outfileS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/fastQC/summary.txt";
+        File[] fs = new File(inputDirS).listFiles();
+        fs = IOUtils.listFilesEndsWith(fs, ".zip");
+        String[][] result = null;
+        String[] column = null;
+        String[] fileNames = new String[fs.length];
+        for (int i = 0; i < fs.length; i++) {
+            try {
+                ZipFile zf = new ZipFile(fs[i].getAbsolutePath());
+                Enumeration<? extends ZipEntry> entries = zf.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry ze = entries.nextElement();
+                    if (!ze.getName().endsWith("summary.txt")) continue;
+                    if ( i == 0) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(zf.getInputStream(ze), "UTF-8"));
+                        String temp = null;
+                        ArrayList<String> columnList = new ArrayList();
+                        while ((temp = br.readLine()) != null) {
+                            String[] tem = temp.split("\t");
+                            columnList.add(tem[1]);
+                        }
+                        column = columnList.toArray(new String[columnList.size()]);
+                        result = new String[fs.length][column.length];
+                        br.close();
+                    }
+                    BufferedReader br = new BufferedReader(new InputStreamReader(zf.getInputStream(ze), "UTF-8"));
+                    String temp = null;
+                    ArrayList<String> columnList = new ArrayList();
+                    int cnt = 0;
+                    while ((temp = br.readLine()) != null) {
+                        String[] tem = temp.split("\t");
+                        result[i][cnt] = tem[0];
+                        fileNames[i] = tem[2];
+                        cnt++;
+                    }
+                    br.close();
+                    break;    
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            StringBuilder sb = new StringBuilder("FileName");
+            for (int i = 0; i < column.length; i++) {
+                sb.append("\t").append(column[i]);
+            }
+            bw.write(sb.toString());
+            bw.newLine();
+            for (int i = 0; i < result.length; i++) {
+                sb = new StringBuilder(fileNames[i]);
+                for (int j = 0; j < column.length; j++) {
+                    sb.append("\t").append(result[i][j]);
+                }
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void fastQC () {
+        String inputDirS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/sampleSeq";
+        String outputDirS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/fastQC/autoReport";
+        try {
+            StringBuilder sb = new StringBuilder("/Users/feilu/Software/FastQC/fastqc");
+            File[] fs = new File (inputDirS).listFiles();
+            fs =  IOUtils.listFilesEndsWith(fs, ".gz");
+            for (int i = 0; i < fs.length; i++) {
+                sb.append(" ").append(fs[i].getAbsoluteFile());
+            }
+            sb.append(" -o ").append(outputDirS);
+            String cmd = sb.toString();
+            System.out.println(cmd);
+            Runtime run = Runtime.getRuntime();
+            Process p = run.exec(cmd);
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String temp = null;
+            while ((temp = br.readLine()) != null) {
+                System.out.println(temp);
+            }
+            p.waitFor();
+            System.out.println("Fastqc evalutation is finished at" + outputDirS);
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
     }
     
     private void sampleReads () {
         String infileDirS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/rawdata";
         String outputDirS = "/Users/feilu/Documents/analysisL/pipelineTest/maize2k/sampleSeq";
-        int readNum = 10000;
+        int readNum = 100000;
         int startPoint = 1000000;
         File[] fs = new File(infileDirS).listFiles();
         HashSet<String> nameSet = new HashSet();
@@ -59,8 +162,10 @@ public class FastqQuality {
                         br2.readLine();br2.readLine();br2.readLine();br2.readLine();
                     }
                     else {
-                        for (int i = 0; i < readNum; i++) {
-                            bw1.write(temp+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");
+                        bw1.write(temp+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");
+                        bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");
+                        for (int i = 0; i < readNum-1; i++) {
+                            bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");bw1.write(br1.readLine()+"\n");
                             bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");bw2.write(br2.readLine()+"\n");
                         }
                         bw1.flush();bw1.close();
