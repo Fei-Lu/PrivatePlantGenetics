787cabfb598f693dd922dd4b3e6bcfcf8e259a5a
Fei Lu <dr.lufei@gmail.com>
2018-03-28 20:36:47 +0800

    HapScanner more

--- src/analysis/maize2k/HapScanner.java
+++ src/analysis/maize2k/HapScanner.java
@@ -5,10 +5,22 @@
  */
 package analysis.maize2k;
 
+import static cern.jet.math.Arithmetic.factorial;
+import com.koloboke.collect.map.IntDoubleMap;
+import com.koloboke.collect.map.hash.HashIntDoubleMaps;
+import format.table.RowTable;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import zhouyao.analysis.wheatHapMap.IOUtils;
+import java.util.Set;
+import java.util.concurrent.atomic.LongAdder;
+import utils.IOUtils;
+import utils.PStringUtils;
 
 /**
  *
@@ -17,12 +29,12 @@ import zhouyao.analysis.wheatHapMap.IOUtils;
 public class HapScanner {
     //The path of reference genome with .fai file in the same folder
     String referenceGenomeS = null;
-    //The directory of sorted bam files, with .bai files in the same folder
-    String bamDirS = null;
-    //The directory of posAllele files (with header), the format is Chr\tPos\tRef\tAlt (from VCF format). The positions come from haplotype library.
-    String posAlleleDirS = null;
-    //The directory of pos files (without header), the format is Chr\tPos. The positions come from haplotype library.
-    String posDirS = null;
+    //The taxaBam file containing information of taxon and its corresponding bam files. The bam file should have .bai file in the same folder
+    String taxaBamFileS = null;
+    //The posAllele file (with header), the format is Chr\tPos\tRef\tAlt (from VCF format). The positions come from haplotype library.
+    String posAlleleFileS = null;
+    //The pos files (without header), the format is Chr\tPos. The positions come from haplotype library.
+    String posFileS = null;
     //The chromosome which will be genotyped
     int chr = -1;
     //The path of samtools
@@ -30,8 +42,282 @@ public class HapScanner {
     //The directory of output
     String outputDirS = null;
     
+    HashMap<String, List<String>> taxaBamsMap = new HashMap<>();
+    
+    String[] subDirS = {"mpileup", "indiVCF", "VCF"};
+    
+    IntDoubleMap factorialMap = null;
+    int maxFactorial = 150;
+    double sequencingErrorRate = 0.05;
+    
     public HapScanner (String infileS) {
         this.parseParameters(infileS);
+        this.mkDir();
+        //this.scanIndiVCF();
+        this.mkFinalVCF();
+    }
+    
+    public void mkFinalVCF () {
+        Set<String> taxaSet = taxaBamsMap.keySet();
+        String[] taxa = taxaSet.toArray(new String[taxaSet.size()]);
+        Arrays.sort(taxa);
+        String outfileS = new File(outputDirS, subDirS[2]).getAbsolutePath();
+        outfileS = new File(outfileS, "chr"+PStringUtils.getNDigitNumber(3, chr)+".vcf").getAbsolutePath();
+        try {
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            bw.write("##fileformat=VCFv4.1\n");
+            bw.write("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n");
+            bw.write("##FORMAT=<ID=AD,Number=.,Type=Integer,Description=\"Allelic depths for the reference and alternate alleles in the order listed\">\n");
+            bw.write("##FORMAT=<ID=GL,Number=.,Type=Integer,Description=\"Genotype likelihoods for 0/0, 0/1, 1/1, or  0/0, 0/1, 0/2, 1/1, 1/2, 2/2 if 2 alt alleles\">\n");
+            bw.write("##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">\n");
+            bw.write("##INFO=<ID=NZ,Number=1,Type=Integer,Description=\"Number of taxa with called genotypes\">\n");
+            bw.write("##INFO=<ID=AD,Number=.,Type=Integer,Description=\"Total allelelic depths in order listed starting with REF\">\n");
+            bw.write("##INFO=<ID=AC,Number=.,Type=Integer,Description=\"Numbers of ALT alleles in order listed\">\n");
+            bw.write("##INFO=<ID=GN,Number=.,Type=Integer,Description=\"Number of taxa with genotypes AA,AB,BB or AA,AB,AC,BB,BC,CC if 2 alt alleles\">\n");
+            bw.write("##INFO=<ID=HT,Number=1,Type=Integer,Description=\"Number of heterozygotes\">\n");
+            bw.write("##INFO=<ID=MAF,Number=1,Type=Float,Description=\"Minor allele frequency\">\n");
+            bw.write("##ALT=<ID=DEL,Description=\"Deletion\">\n");
+            bw.write("##ALT=<ID=INS,Description=\"Insertion\">\n");
+            bw.write("##HapMapVersion=\"3.2.1\"\n");
+            StringBuilder sb = new StringBuilder("#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT");
+            for (int i = 0; i < taxa.length; i++) {
+                sb.append("\t").append(taxa[i]);
+            }
+            bw.write(sb.toString());
+            bw.newLine();
+            BufferedReader[] brs = new BufferedReader[taxa.length];
+            String indiVCFFolderS = new File(outputDirS, subDirS[1]).getAbsolutePath();
+            for (int i = 0; i < brs.length; i++) {
+                String indiVCFFileS = new File (indiVCFFolderS, taxa[i]+".indi.vcf").getAbsolutePath();
+                brs[i] = IOUtils.getTextReader(indiVCFFileS);
+            }
+            BufferedReader br = IOUtils.getTextGzipReader(posAlleleFileS);
+            String temp = br.readLine();
+            List<String> temList = null;
+            while ((temp = br.readLine()) != null) {
+                sb = new StringBuilder();
+                temList = PStringUtils.fastSplit(temp);
+                sb.append(temList.get(0)).append("\t").append(temList.get(1)).append("\t").append(temList.get(0)).append("-").append(temList.get(1)).append("\t");
+                sb.append(temList.get(2)).append("\t").append(temList.get(3)).append("\t.\t.\t");
+                List<String> genoList = new ArrayList<>();
+                for (int i = 0; i < brs.length; i++) {
+                    genoList.add(brs[i].readLine());
+                }
+                sb.append(this.getInfo(genoList)).append("\tGT:AD:GL");
+                for (int i = 0; i < genoList.size(); i++) {
+                    sb.append("\t").append(genoList.get(i));
+                }
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+            br.close();
+            for (int i = 0; i < brs.length; i++) {
+                brs[i].close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private String getInfo (List<String> genoList) {
+        int dp = 0;
+        int nz = 0;
+        return "DP=";
+    }
+    
+    public void scanIndiVCF () {
+        this.creatFactorialMap();
+        RowTable<String> t = new RowTable<> (posAlleleFileS);
+        HashMap<Integer, String> posRefMap = new HashMap<>();
+        HashMap<Integer, String[]> posAltMap = new HashMap<>();
+        int[] positions = new int[t.getRowNumber()];
+        for (int i = 0; i < t.getRowNumber(); i++) {
+            positions[i] = t.getCellAsInteger(i, 1);
+            posRefMap.put(positions[i], t.getCell(i, 2));
+            String[] tem = t.getCell(i, 3).split(",");
+            posAltMap.put(t.getCellAsInteger(i, 1), tem);
+        }
+        Set<String> taxaSet = taxaBamsMap.keySet();
+        LongAdder counter = new LongAdder();
+        taxaSet.parallelStream().forEach(e -> {
+            String pileupFolderS = new File(outputDirS, subDirS[0]).getAbsolutePath();
+            String pileupFileS = new File (pileupFolderS, e+".pileup.txt").getAbsolutePath();
+            String indiVCFFolderS = new File(outputDirS, subDirS[1]).getAbsolutePath();
+            String indiVCFFileS = new File (indiVCFFolderS, e+".indi.vcf").getAbsolutePath();
+            List<String> bamPaths = taxaBamsMap.get(e);
+            StringBuilder sb = new StringBuilder(samtoolsPath);
+            sb.append(" mpileup -A -B -q 20 -Q 20 -f ").append(referenceGenomeS);
+            for (int i = 0; i < bamPaths.size(); i++) {
+                sb.append(" ").append(bamPaths.get(i));
+            }
+            sb.append(" -l ").append(posFileS).append(" -r ");
+            sb.append(chr).append(" -o ").append(pileupFileS);
+            String command = sb.toString();
+            //System.out.println(command);
+            try {
+                Runtime rt = Runtime.getRuntime();
+                Process p = rt.exec(command);
+                p.waitFor();
+            }
+            catch (Exception ee) {
+                ee.printStackTrace();
+            }
+            counter.increment();
+            int cnt = counter.intValue();
+            if (cnt%10 == 0) System.out.println("Pileuped " + String.valueOf(cnt) + " taxa. Total: " + String.valueOf(taxaBamsMap.size()));
+            try {
+                BufferedReader br = IOUtils.getTextReader(pileupFileS);
+                BufferedWriter bw = IOUtils.getTextWriter(indiVCFFileS);
+                String current = br.readLine();
+                List<String> currentList = null;
+                int currentPosition = -1;
+                if (current != null) {
+                    currentList = PStringUtils.fastSplit(current);
+                    currentPosition = Integer.parseInt(currentList.get(1));
+                }
+                for (int i = 0; i < positions.length; i++) {
+                    if (current == null) {
+                        bw.write("./.");
+                        bw.newLine();
+                    }
+                    else {
+                        if (positions[i] == currentPosition) {
+                            String ref = posRefMap.get(currentPosition);
+                            String[] alts = posAltMap.get(currentPosition);
+                            char[] alleleC = new char[alts.length+1];
+                            alleleC[0] = ref.charAt(0);
+                            for (int j = 0; j < alts.length; j++) {
+                                if (alts[j].startsWith("<I")) {
+                                    alleleC[j+1] = '+';
+                                }
+                                else if (alts[j].startsWith("<D")) {
+                                    alleleC[j+1] = '-';
+                                }
+                                alleleC[j+1] = alts[j].charAt(0);
+                            }
+                            int[] cnts = new int[alts.length+1];
+                            sb = new StringBuilder();
+                            for (int j = 0; j < bamPaths.size(); j++) {
+                                sb.append(currentList.get(4+j*3));
+                            }
+                            StringBuilder ssb = new StringBuilder();
+                            int curIndex = 0;
+                            for (int j = 0; j < sb.length(); j++) {
+                                char cChar = sb.charAt(j);
+                                if (cChar == '+') {
+                                    ssb.append(sb.subSequence(curIndex, j+1));
+                                    curIndex = j+2+Character.getNumericValue(sb.charAt(j+1));
+                                }
+                                else if (cChar == '-') {
+                                    ssb.append(sb.subSequence(curIndex, j+1));
+                                    curIndex = j+2+Character.getNumericValue(sb.charAt(j+1));
+                                }
+                            }
+                            ssb.append(sb.subSequence(curIndex, sb.length()));
+                            sb = ssb;
+                            String s = sb.toString().toUpperCase();
+                            for (int j = 0; j < s.length(); j++) {
+                                char cChar = s.charAt(j);
+                                if (cChar == '.' || cChar == ',') cnts[0]++;
+                                for (int k = 1; k < alleleC.length; k++) {
+                                    if (cChar == alleleC[k]) cnts[k]++;
+                                }
+                            }
+                            for (int j = 1; j < alleleC.length; j++) {
+                                if (alleleC[j] == '+') cnts[0] = cnts[0]-cnts[j];
+                                else if (alleleC[j] == '-') cnts[0] = cnts[0]-cnts[j];
+                            }
+                            String vcf = this.getGenotype(cnts);
+                            bw.write(vcf);
+                            bw.newLine();
+
+                            current = br.readLine();
+                            if (current != null) {
+                                currentList = PStringUtils.fastSplit(current);
+                                currentPosition = Integer.parseInt(currentList.get(1));
+                            } 
+                        }
+                        else if (positions[i] < currentPosition) {
+                            bw.write("./.");
+                            bw.newLine();    
+                        }
+                        else {
+                            System.out.println("Currentposition is greater than pileup position. It should not happen. Program quits");
+                            System.exit(1);
+                        }
+                    }
+                }
+                bw.flush();
+                bw.close();
+                br.close();
+            }
+            catch (Exception ee) {
+                ee.printStackTrace();
+                System.exit(1);
+            }
+        });
+    }
+    
+    private String getGenotype (int[] cnt) {
+        int n = cnt.length*(cnt.length+1)/2;
+        int[] likelihood = new int[n];
+        int sum = 0;
+        for (int i = 0; i < cnt.length; i++) sum+=cnt[i];
+        if (sum == 0) return "./.";
+        else if (sum > this.maxFactorial) {
+            double portion = (double)this.maxFactorial/sum;
+            for (int i = 0; i < cnt.length; i++) {
+                cnt[i] = (int)(cnt[i]*portion);
+            }
+            sum = this.maxFactorial;
+        }
+        double coe = this.factorialMap.get(sum);
+        for (int i = 0; i < cnt.length; i++) coe = coe/this.factorialMap.get(cnt[i]);
+        double max = Double.MAX_VALUE;
+        int a1 = 0;
+        int a2 = 0;
+        for (int i = 0; i < cnt.length; i++) {
+            for (int j = i; j < cnt.length; j++) {
+                int index = (j*(j+1)/2)+i;
+                double value = Double.MAX_VALUE;
+                if (i == j) {
+                    value = -Math.log10(coe*Math.pow((1-0.75*this.sequencingErrorRate), cnt[i])*Math.pow(this.sequencingErrorRate/4, (sum-cnt[i])));
+                }
+                else {
+                    value = -Math.log10(coe*Math.pow((0.5-this.sequencingErrorRate/4), cnt[i]+cnt[j])*Math.pow(this.sequencingErrorRate/4, (sum-cnt[i]-cnt[j])));
+                }
+                if (value < max) {
+                    max = value;
+                    a1 = i;
+                    a2 = j;
+                }
+                likelihood[index] = (int)Math.round(value);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(a1).append("/").append(a2).append(":");
+        for (int i = 0; i < cnt.length; i++) sb.append(cnt[i]).append(",");
+        sb.deleteCharAt(sb.length()-1); sb.append(":");
+        for (int i = 0; i < likelihood.length; i++) sb.append(likelihood[i]).append(",");
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+    
+    private void creatFactorialMap () {
+        this.factorialMap = HashIntDoubleMaps.getDefaultFactory().newMutableMap();
+        for (int i = 0; i < this.maxFactorial+1; i++) {
+            this.factorialMap.put(i, factorial(i));
+        }
+    }
+    
+    public void mkDir () {
+        for (int i = 0; i < subDirS.length; i++) {
+            File f = new File(outputDirS, subDirS[i]);
+            f.mkdir();
+        }
     }
     
     public void parseParameters (String infileS) {
@@ -60,12 +346,30 @@ public class HapScanner {
             System.exit(1);
         }
         referenceGenomeS = pLineList.get(0);
-        bamDirS = pLineList.get(1);
-        posAlleleDirS = pLineList.get(2);
-        posDirS = pLineList.get(3);
+        taxaBamFileS = pLineList.get(1);
+        posAlleleFileS = pLineList.get(2);
+        posFileS = pLineList.get(3);
         chr = Integer.valueOf(pLineList.get(4));
         samtoolsPath = pLineList.get(5);
-        outputDirS = pLineList.get(6);   
+        outputDirS = pLineList.get(6);
+        RowTable<String> t = new RowTable<>(taxaBamFileS);
+        Set<String> taxaSet = new HashSet<>();
+        for (int i = 0; i < t.getRowNumber(); i++) {
+            taxaSet.add(t.getCell(i, 0));
+        }
+        String[] taxaArray = taxaSet.toArray(new String[taxaSet.size()]);
+        Arrays.sort(taxaArray);
+        for (int i = 0; i < taxaArray.length; i++) {
+            List<String> bamList = new ArrayList<>();
+            taxaBamsMap.put(taxaArray[i], bamList);
+        }
+        for (int i = 0; i < t.getRowNumber(); i++) {
+            String key = t.getCell(i, 0);
+            List<String> bamList = taxaBamsMap.get(key);
+            bamList.add(t.getCell(i, 1));
+            taxaBamsMap.replace(key, bamList);
+        }
+        int a = 3;
     }
     
 }
--- src/analysis/maize2k/Maize2kGo.java
+++ src/analysis/maize2k/Maize2kGo.java
@@ -5,6 +5,11 @@
  */
 package analysis.maize2k;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import utils.IOUtils;
+
+
 /**
  *
  * @author feilu
@@ -15,12 +20,33 @@ public class Maize2kGo {
         //this.processReference();
         //this.seqQualityTestPipe();
         this.setHapScannerPipe();
+        //this.test();
     }
     
     private void setHapScannerPipe () {
         //new HapMap3Processor();
         String parameterFileS = "/Users/feilu/Documents/analysisL/pipelineTest/HapScanner/parameters_hapScanner.txt";
         new HapScanner(parameterFileS);
+    }
+    
+    public void test () {
+        String infileS = "/Users/feilu/Documents/analysisL/pipelineTest/HapScanner/hapMap3_AGPV4/hmp321_agpv4_chr10.vcf.gz";
+        String outfileS = "/Users/feilu/Documents/analysisL/pipelineTest/HapScanner/test.vcf";
+        int length = 10000;
+        try {
+            BufferedReader br = IOUtils.getTextGzipReader(infileS);
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            for (int i = 0; i < length; i++) {
+                bw.write(br.readLine());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     private void seqQualityTestPipe () {
