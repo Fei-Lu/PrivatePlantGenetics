363b491efabb6bc90caf81fa8c616a6ef4b89b3d
Fei Lu <dr.lufei@gmail.com>
2019-11-22 01:57:06 +0800

    wheat gene expression table

--- src/analysis/publicData/WheatReferenceGene.java
+++ src/analysis/publicData/WheatReferenceGene.java
@@ -6,13 +6,16 @@
 package analysis.publicData;
 
 import format.genomeAnnotation.GeneFeature;
+import format.range.Range;
 import format.table.RowTable;
+import gnu.trove.list.array.TIntArrayList;
 import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import utils.Benchmark;
 import utils.IOUtils;
 
@@ -25,14 +28,101 @@ public class WheatReferenceGene {
     public WheatReferenceGene () {
         //this.GFFToPGF();
         //this.mkExpressionByTissue();
+        //this.mkExpressionSummary();
         this.mkExpressedGene();
     }
     
     public void mkExpressedGene () {
         String pgfFileS = "/Users/feilu/Documents/database/wheat/gene/v1.1/wheat_v1.1_Lulab.pgf";
-        String expressionInFileS = "/Users/feilu/Documents/database/wheat/gene/gene_expression/download/Development_tpm.tsv.gz";
-        String header = "Gene\tOverlapped_gene\tLongest_overlapped_gene\tLongest_transcript\tTPM_mean\tTPM_sd\tTPM_rsd";
-        
+        String infileS = "/Users/feilu/Documents/database/wheat/gene/gene_expression/expressionByTissueSummary.txt";
+        String outfileS = "/Users/feilu/Documents/database/wheat/gene/gene_expression/geneExpression.txt";
+        String header = "Gene\tIs_Overlapped_gene(1,0)\tNumber_Overlapped_gene\tIs_Unique_gene(1,0)\tLongest_transcript\tTPM_mean\tTPM_sd\tTPM_rsd";
+        RowTable<String> t = new RowTable(infileS);
+        t.sortAsText(0);
+        List<String> tList = t.getColumn(0);
+        GeneFeature gf = new GeneFeature (pgfFileS);
+        Range[] geneRanges = new Range[gf.getGeneNumber()];
+        for (int i = 0; i < gf.getGeneNumber(); i++) {
+            geneRanges[i] = new Range(gf.getGeneChromosome(i), gf.getGeneStart(i), gf.getGeneEnd(i));
+        }
+        try {
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            bw.write(header);
+            bw.newLine();
+            TIntArrayList indexList = new TIntArrayList();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < geneRanges.length; i++) {
+                indexList.clear();
+                sb.setLength(0);
+                sb.append(gf.getGeneName(i)).append("\t");
+                for (int j = 0; j < geneRanges.length; j++) {
+                    if (i == j) continue;
+                    if (geneRanges[i].getRangeChromosome() != geneRanges[j].getRangeChromosome()) continue;
+                    if (geneRanges[i].isOverlap(geneRanges[j])) {
+                        indexList.add(j);
+                    }
+                }
+                if (indexList.size() == 0) {
+                    sb.append(0).append("\t").append(0).append("\t").append(1).append("\t");
+                }
+                else {
+                    sb.append(1).append("\t").append(indexList.size()).append("\t");
+                    int value = 1;
+                    for (int j = 0; j < indexList.size(); j++) {
+                        if (geneRanges[i].getRangeSize() < geneRanges[j].getRangeSize()) {
+                            value = 0;
+                            break;
+                        }
+                    }
+                    sb.append(value).append("\t");
+                }
+                String tName = gf.getTranscriptName(i, gf.getLongestTranscriptIndex(i));
+                sb.append(tName).append("\t");
+                int index = Collections.binarySearch(tList, tName);
+                sb.append(t.getCell(index, 1)).append("\t").append(t.getCell(index, 2)).append("\t").append(t.getCell(index, 3));
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void mkExpressionSummary () {
+        String infileS = "/Users/feilu/Documents/database/wheat/gene/gene_expression/expressionByTissue.txt";
+        String outfileS = "/Users/feilu/Documents/database/wheat/gene/gene_expression/expressionByTissueSummary.txt";
+        RowTable<String> t = new RowTable(infileS);
+        String header = "Transcripts\tTPM_mean\tTPM_sd\tTPM_rsd";
+        try {
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            bw.write(header);
+            bw.newLine();
+            double[] values = new double[t.getColumnNumber()-1];
+            StringBuilder sb = new StringBuilder();
+            DescriptiveStatistics ds = null;
+            for (int i = 0; i < t.getRowNumber(); i++) {
+                sb.setLength(0);
+                sb.append(t.getCell(i, 0));
+                for (int j = 0; j < values.length; j++) {
+                    values[j] = t.getCellAsDouble(i, j+1);
+                }
+                ds = new DescriptiveStatistics(values);
+                double mean = ds.getMean();
+                double sd = ds.getStandardDeviation();
+                double rsd = sd/mean;
+                sb.append("\t").append((float)mean).append("\t").append((float)sd).append("\t").append((float)rsd);
+                bw.write(sb.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public void mkExpressionByTissue () {
