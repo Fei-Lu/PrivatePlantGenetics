4a1d32a9cf4375a3712b4d0a44b3dbb96ba47b80
Fei Lu <dr.lufei@gmail.com>
2019-11-26 20:33:08 +0800

    genic site VMap

--- src/analysis/wheatHapMap/DeleteriousDB.java
+++ src/analysis/wheatHapMap/DeleteriousDB.java
@@ -5,15 +5,20 @@
  */
 package analysis.wheatHapMap;
 
-import format.genomeAnnotation.GeneFeature;
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
 import graphcis.tablesaw.TablesawUtils;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import tech.tablesaw.api.IntColumn;
 import tech.tablesaw.api.Table;
-import tech.tablesaw.plotly.Plot;
-import tech.tablesaw.plotly.api.Histogram;
-import tech.tablesaw.plotly.api.ScatterPlot;
-import tech.tablesaw.plotly.components.Figure;
+import utils.IOUtils;
+import utils.PStringUtils;
 /**
  *
  * @author feilu
@@ -25,6 +30,96 @@ public class DeleteriousDB {
     }
     
     public void extractInfoFromVMap2 () {
+        int subLength = 150;
+        String outDirS = "/Users/feilu/Documents/analysisH/vmap2/002_genicSNP/genicSNPByChr/";
+        String vmapDirS = "/Volumes/Fei_HDD_Mac/VMap2.1/";
+        File[] fs  = new File(vmapDirS).listFiles();
+        fs = IOUtils.listFilesEndsWith(fs, ".gz");
+        List<File> vmapList = Arrays.asList(fs);
+        Collections.sort(vmapList);
+        String geneHCFileS = "/Users/feilu/Documents/analysisH/vmap2/001_geneHC/geneHC.txt";
+        Table t = TablesawUtils.readTsv(geneHCFileS);
+        System.out.println(t.structure());
+        t.sortAscendingOn("Chr", "TranStart");
+        IntColumn chrColumn = t.intColumn("chr");
+        int chrNum = chrColumn.countUnique();
+        TIntList[] startLists = new TIntList[chrNum];
+        TIntList[] endLists = new TIntList[chrNum];
+        List<String>[] tranLists = new ArrayList[chrNum];
+        for (int i = 0; i < chrNum; i++) {
+            startLists[i] = new TIntArrayList();
+            endLists[i] = new TIntArrayList();
+            tranLists[i] = new ArrayList();
+        }
+        for (int i = 0; i < t.rowCount(); i++) {
+            startLists[Integer.parseInt(t.getString(i, 2))-1].add(Integer.parseInt(t.getString(i, 3)));
+            endLists[Integer.parseInt(t.getString(i, 2))-1].add(Integer.parseInt(t.getString(i, 4)));
+            tranLists[Integer.parseInt(t.getString(i, 2))-1].add(t.getString(i, 1));
+        }
+        vmapList.parallelStream().forEach(f -> {
+            int chrIndex = Integer.parseInt(f.getName().substring(3, 6))-1;
+            String outfileS = new File (outDirS, f.getName().replaceFirst(".vcf.gz", "genicSNP.txt.gz")).getAbsolutePath();
+            int[] dc = {5, 6, 11, 12, 17, 18, 23, 24, 29, 30, 35, 36, 41, 42};
+            Arrays.sort(dc);
+            StringBuilder sb = new StringBuilder();
+            if (Arrays.binarySearch(dc, chrIndex+1) < 0) {
+                sb.append("ID\tChr\tPos\tRef\tAlt\tMajor\tMinor\tMaf\tAAF_ABD\tAAF_AB\tTranscript");
+            }
+            else {
+                sb.append("ID\tChr\tPos\tRef\tAlt\tMajor\tMinor\tMaf\tAAF_ABD\tAAF_D\tTranscript");
+            }
+            try {
+                BufferedReader br = IOUtils.getTextGzipReader(f.getAbsolutePath());
+                BufferedWriter bw = IOUtils.getTextGzipWriter(outfileS);
+                bw.write(sb.toString());
+                bw.newLine();
+                String temp = null;
+                while ((temp = br.readLine()).startsWith("#")) {}
+                
+                List<String> l = null;
+                List<String> ll = null;
+                List<String> lll = null;
+                String info = null;
+                int currentPos = -1;
+                int posIndex = -1;
+                while ((temp = br.readLine()) != null) {
+                    sb.setLength(0);
+                    int currentSub = subLength;
+                    if (temp.length() < subLength) {
+                        currentSub = temp.length();
+                    }
+                    l = PStringUtils.fastSplit(temp.substring(0, currentSub));
+                    currentPos = Integer.parseInt(l.get(1));
+                    posIndex = startLists[chrIndex].binarySearch(currentPos);
+                    if (posIndex < 0) {
+                        posIndex = -posIndex-2;
+                    }
+                    if (posIndex < 0) continue;
+                    if (currentPos >= endLists[chrIndex].get(posIndex)) continue;
+                    sb.append(l.get(2)).append("\t").append(l.get(0)).append("\t").append(l.get(1)).append("\t").append(l.get(3));
+                    sb.append("\t").append(l.get(4)).append("\t");
+                    ll = PStringUtils.fastSplit(l.get(7), ";");
+                    lll = PStringUtils.fastSplit(ll.get(2).replaceFirst("AD=", ""),",");
+                    if (Integer.parseInt(lll.get(0)) > Integer.parseInt(lll.get(1))) {
+                        sb.append(l.get(3)).append("\t").append(l.get(4)).append("\t");
+                    }
+                    else {
+                        sb.append(l.get(4)).append("\t").append(l.get(3)).append("\t");
+                    }
+                    sb.append(ll.get(6).split("=")[1]).append("\t").append(ll.get(7).split("=")[1]).append("\t").append(ll.get(8).split("=")[1]);
+                    sb.append("\t").append(tranLists[chrIndex].get(posIndex));
+                    bw.write(sb.toString());
+                    bw.newLine();
+                }
+                bw.flush();
+                bw.close();
+                br.close();
+                System.out.println(f.getAbsolutePath() + " is completed.");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         
     }
 }
