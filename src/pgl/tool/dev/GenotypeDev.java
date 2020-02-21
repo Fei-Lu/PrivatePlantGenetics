56454e65569fbfd985bd0fb4aa3cd1bb92b15279
Fei Lu <dr.lufei@gmail.com>
2020-02-22 01:20:28 +0800

    Genotype Table binary reader writer

--- src/pgl/tool/dev/GenotypeDev.java
+++ src/pgl/tool/dev/GenotypeDev.java
@@ -5,22 +5,40 @@ import pgl.infra.dna.allele.AlleleEncoder;
 import pgl.infra.dna.allele.AlleleType;
 import pgl.infra.dna.genotype.GenoIOFormat;
 import pgl.infra.dna.genotype.GenotypeBit;
+import pgl.infra.dna.genotype.GenotypeExport;
 import pgl.infra.dna.genotype.GenotypeTable;
+
+import java.nio.ByteBuffer;
+import java.util.BitSet;
 
 public class GenotypeDev {
 
     public GenotypeDev () {
         //this.testAlleles();
         this.readVCF();
+        BitSet bs = new BitSet(65);
+        bs.set(65);
+
+        long[] bl = bs.toLongArray();
+        byte[] ba = bs.toByteArray();
+        int v = bs.size();
+        int a = 3;
+        
     }
 
     public void readVCF () {
         String vcfFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/genotype/chr001_exon_vmap2.1.vcf.gz";
-        GenotypeTable gt  = new GenotypeBit(vcfFileS, GenoIOFormat.VCF);
-        System.out.println(gt.getMinorAlleleFrequency(1));
-        gt.sortByTaxa();
-        System.out.println(gt.getMinorAlleleFrequency(1));
-        
+//        String vcfFileS = "/Volumes/Fei_HDD_Mac/VMap2.1/chr001_vmap2.1.vcf.gz";
+        GenotypeTable gt  = new GenotypeBit(vcfFileS, GenoIOFormat.VCF_GZ);
+
+        String vcfOutFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/genotype/out.vcf";
+        GenotypeExport.output(gt, vcfOutFileS, GenoIOFormat.VCF);
+
+        String binOutFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/genotype/out.bin.gz";
+        GenotypeExport.output(gt, binOutFileS, GenoIOFormat.Binary_GZ);
+
+        gt  = new GenotypeBit(binOutFileS, GenoIOFormat.Binary_GZ);
+        GenotypeExport.output(gt, vcfOutFileS, GenoIOFormat.VCF);
 
     }
 
