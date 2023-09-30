fc89ce5c212e63edbb24ad3b5fa235ae27da3b49
Fei Lu (鲁非) <dr.lufei@gmail.com>
2023-09-30 23:02:52 +0800

    Fixing first read sign "^" in FastCall2

--- src/pgl/tool/dev/FastCall2Dev.java
+++ src/pgl/tool/dev/FastCall2Dev.java
@@ -7,7 +7,6 @@ import pgl.app.fastCall2.FastCall2;
 class FastCall2Dev {
 
     public FastCall2Dev () {
-//        this.withParameterFileS();
         this.withCommandLine();
     }
 
@@ -23,7 +22,7 @@ class FastCall2Dev {
     public void commandDiscovery () {
         StringBuilder sb = new StringBuilder();
         sb.append("-app ").append("FastCall2 ");
-        sb.append("-tool ").append("disc ");
+        sb.append("-module ").append("disc ");
         sb.append("-a ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall/ref/chr001_1Mb.fa ");
         sb.append("-b ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall/inputfile/taxaBamMap.txt ");
         sb.append("-c ").append("30 ");
@@ -45,7 +44,7 @@ class FastCall2Dev {
     public void commandLibrary () {
         StringBuilder sb = new StringBuilder();
         sb.append("-app ").append("FastCall2 ");
-        sb.append("-tool ").append("blib ");
+        sb.append("-module ").append("blib ");
         sb.append("-a ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall/ref/chr001_1Mb.fa ");
         sb.append("-b ").append("1:1,200000 ");
         sb.append("-c ").append("2 ");
@@ -59,40 +58,19 @@ class FastCall2Dev {
     public void commandGenotype () {
         StringBuilder sb = new StringBuilder();
         sb.append("-app ").append("FastCall2 ");
-        sb.append("-tool ").append("scan ");
+        sb.append("-module ").append("scan ");
         sb.append("-a ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall/ref/chr001_1Mb.fa ");
         sb.append("-b ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall/inputfile/taxaBamMap.txt ");
         sb.append("-c ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/vLib/1_1_200001.lib.gz ");
         sb.append("-d ").append("1:1,200000 ");
-        sb.append("-e ").append("0.05 ");
-        sb.append("-f ").append("/usr/local/bin/samtools ");
-        sb.append("-g ").append("32 ");
-        sb.append("-h ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/gen/ ");
+        sb.append("-e ").append("30 ");
+        sb.append("-f ").append("20 ");
+        sb.append("-g ").append("0.05 ");
+        sb.append("-h ").append("/usr/local/bin/samtools ");
+        sb.append("-i ").append("32 ");
+        sb.append("-j ").append("/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/gen/ ");
         String[] args = sb.toString().split(" ");
         new FastCall2(args);
     }
 
-//    /**
-//     * @deprecated
-//     */
-//    public void withParameterFileS () {
-//        this.callIndividual();
-//        this.mkVariationLib();
-//        this.genotyping();
-//    }
-//
-//    public void genotyping () {
-//        String parameterFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/inputfile/parameters_fastcall2_3.txt";
-//        new FastCall2(parameterFileS);
-//    }
-//
-//    public void mkVariationLib () {
-//        String parameterFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/inputfile/parameters_fastcall2_2.txt";
-//        new FastCall2(parameterFileS);
-//    }
-//
-//    public void callIndividual () {
-//        String parameterFileS = "/Users/feilu/Documents/analysisL/softwareTest/pgl/fastCall2/inputfile/parameters_fastcall2_1.txt";
-//        new FastCall2(parameterFileS);
-//    }
 }
